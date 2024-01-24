d8706832a78a8ad18e62b9c9c07ecf7f639b21c3
Vladimir F <83063876+Anxal@users.noreply.github.com>
2024-01-24 22:35:00 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests, openai
 
-html = requests.get("https://www.m.gortransperm.ru").text
+html = requests.get("https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/").text
+
 
 print(html)
 
