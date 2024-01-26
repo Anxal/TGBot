aee88d03c574146558e5af799d78987a04281dff
Vladimir F <83063876+Anxal@users.noreply.github.com>
2024-01-26 23:44:08 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from bs4 import BeautifulSoup
 import requests, openai
+import csv
 
-html = requests.get("https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/").text
+def parser(url:str):
+    res = requests.get(url=url)
+    soup = BeautifulSoup(res.text,"lxml")
+    bus = soup.find_all("div", class_="ui-content")
+    for bas in bus:
+        namebus = bas.get("ul data-role")
+    print(namebus)
+    print(bus)
 
-
-print(html)
-
+if __name__== "__main__":
+    parser(url="https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/")
