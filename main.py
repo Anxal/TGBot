338fe2b9f4f9d514cf8e4e3d654e417764a3c5fc
Vladimir F <83063876+Anxal@users.noreply.github.com>
2024-01-29 21:41:37 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests, openai, aiogram
 import csv
 import telebot
 
-URL = 'https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/'
+url = 'https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/'
 bot = telebot.TeleBot('6826891756:AAFhGsMbOT-Y2C59XQ1e1uQUsOdwR-_9MTA')
 
 #Функция отправляющая гет запрос
@@ -23,7 +23,7 @@ def sum():
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, parser())
+    bot.send_message(message.chat.id, parser(url))
 
 
 #Бесконечный цикл получения новых записей со стороны Telegram:
