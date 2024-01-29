7e51f64a2a88e2ad1ee8dd7a217c5f4c02382fde
Vladimir F <83063876+Anxal@users.noreply.github.com>
2024-01-29 21:32:26 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from bs4 import BeautifulSoup
-import requests, openai
+import requests, openai, aiogram
 import csv
+import telebot
 
-def parser(url:str):
+URL = 'https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/'
+bot = telebot.TeleBot('6826891756:AAFhGsMbOT-Y2C59XQ1e1uQUsOdwR-_9MTA')
+
+#Функция отправляющая гет запрос
+def parser(url):
     res = requests.get(url=url)
     soup = BeautifulSoup(res.text,"lxml")
-    bus = soup.find_all("div", class_="ui-content")
-    for bas in bus:
-        namebus = bas.get("ul data-role")
-    print(namebus)
-    print(bus)
+    bus_stop = soup.find("div", class_='ui-content').find("h4").text.strip()
+    print(bus_stop)
+    bus_number = soup.find_all("li")
 
-if __name__== "__main__":
-    parser(url="https://www.m.gortransperm.ru/stoppoint-arrival-times/55100/")
+    for bus_time in bus_number:
+        return bus_time.text
+
+def sum():
+    return 2+2
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, parser())
+
+
+#Бесконечный цикл получения новых записей со стороны Telegram:
+if __name__ == '__main__':
+     bot.infinity_polling()
